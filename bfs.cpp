009d2252a63dc36876fb5ab4dc204b6420d7583e
Tusar-Abdullah <tusarabdullah09@gmail.com>
2024-11-27 21:52:59 +0600

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,45 +1,53 @@
-#include<stdio.h>
-#include<iostream>
-#include<queue>
-
+#include<bits/stdc++.h>
 using namespace std;
+
 
 int visited[5];
 
-void bfs(int G[5][5], int node)
+void bfs(int g[5][5],int node)
 {
     queue<int> Q;
     Q.push(node);
 
     visited[node] = 1;
-    printf("%d ", node);
+    cout<<node<<" ";
 
     while(!Q.empty())
-    {
-        int v = Q.front();
-        Q.pop();
-
-        for(int i=0; i<5; i++)
         {
-            if(G[v][i]==1 && visited[i]==0)
-            {
-                Q.push(i);
-                visited[i]=1;
-                printf("%d ", i);
-            }
+            int v = Q.front();
+            Q.pop();
+
+            for(int i=0; i<5; i++)
+                {
+                    if(g[v][i] == 1 && visited[i] == 0)
+                        {
+                            Q.push(i);
+                            visited[i] = 1;
+                            cout<<i<<" ";
+                        }
+                }
         }
-    }
+
+
 }
+
+
+
 
 int main()
 {
-    int G[5][5] =
+
+    int g[5][5] =
     {
         {0,1,0,0,1},
         {1,0,0,1,1},
         {0,0,0,1,0},
         {0,1,1,0,1},
         {1,1,0,1,0}
+
     };
-    bfs(G, 0);
+
+    bfs(g,0);
+
+    return 0;
 }
